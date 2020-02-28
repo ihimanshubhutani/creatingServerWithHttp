609a9017afb67637a5afb8c1f7f161e108dd36c3
Himanshu bhutani <hbhutani@techracers.io>
2020-02-28 21:15:45 +0530

    feature: added Slack API to fetch members and Post message to particular member

---
+++ slackAPI.js
@@ -0,0 +1,48 @@
+const request = require('request');
+const _ = require('lodash');
+const config = require('config');
+
+/**
+ * This function sends message to userid of Slack
+ * @param   userID  {string} Slack id of user to whom message is to be sent.
+ * @param   message {string} Message to be sent to user.
+ * @returns {void}
+ */
+const sendMessage = (userID, message) => {
+
+  config.POST_MESSAGE.json.channel = userID;
+  config.POST_MESSAGE.json.text = message;
+
+  request.post(config.POST_MESSAGE)
+    .on(config.OPTION.ERROR, (error) => console.error(error))
+    .on(config.OPTION.RESPONSE, (response) =>
+      response.on(config.OPTION.DATA, (data) => {
+        const response = JSON.parse(data);
+
+        if (_.isEqual(response.ok, false)) {
+          console.error(` ${response.error}`);
+          return;
+        }
+
+        console.log(config.MESSAGE.SENT);
+      }));
+}
+
+/**
+ * Fetch all members from slack workplace {Right-now from testapipractice.slack.com }
+ * @returns {void}
+ */
+const getUsersList = () => {
+  request.get(config.USER_LIST)
+    .on(config.OPTION.ERROR, (err) => console.error(err))
+    .on(config.OPTION.RESPONSE, (response) =>
+
+      response.on(config.OPTION.DATA, (data) => {
+        const users = JSON.parse(data);
+        const usernameList = _.map(users.members, i => _.pick(i, 'real_name', 'id'));
+        console.log(`${config.MESSAGE.MEMBERS_LIST} ${_.compact(usernameList)}`);
+      }));
+};
+
+sendMessage('UUPRNU82K', 'HELLO THIS IS TEST MESSAGE');
+getUsersList();

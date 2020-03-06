f3f1a646b0b924a13eb2c38f74600ed2c4647462
Himanshu Bhutani <hbhutani@techracers.io>
2020-03-06 06:47:07 +0000

    refactor: renamed directory to path

--- http/routes.js
+++ http/routes.js
@@ -1,17 +1,14 @@
 const fs = require('fs');
 
-const open = (filename) => {
+const read = filename => {
   const fileData = JSON.parse(fs.readFileSync(filename, 'utf8'));
-
   return fileData;
 };
 
-let id;
-
-const directory = {
-  '/': open('./api/index.json', id),
-  '/users': open('./api/users.json', id),
-  '/people': open('./api/people.json', id),
+const routes = {
+  '/': read('./api/index.json'),
+  '/users': read('./api/users.json'),
+  '/people': read('./api/people.json'),
 };
 
-module.exports = directory;
+module.exports = routes;
